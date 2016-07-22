98dc84e10ed58aebf789af25308c79c1a50506c3
Alexon Balangue <moi.sites@alexonbalangue.me>
2016-07-22 14:28:14 +0200

    imported/edited preparing project for new design from admin TLE

---
+++ custom/cpanel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  Templates.AdminLTE
+ *
+ * @copyright   Copyright (C) 2016 Alexon Balangue. All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+require_once __DIR__ . '/index.php';
